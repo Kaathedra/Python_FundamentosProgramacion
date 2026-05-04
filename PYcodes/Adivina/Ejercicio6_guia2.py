6079b963748293f4e02446a05f7d326dbd677b9e
Tomas Canales <tomas.canales.mella@gmail.com>
2026-05-04 14:42:38 -0400

    Reduccion de intentos, añadí sistema de puntuacion

--- PYcodes/Adivina/Ejercicio6_guia2.py
+++ PYcodes/Adivina/Ejercicio6_guia2.py
@@ -1,7 +1,7 @@
 import random as ran
 
 numero_secreto = ran.randint(1, 100)
-intentos_maximos = 10
+intentos_maximos = 5
 
 intentos_realizados = 0
 
@@ -12,12 +12,15 @@ while intentos_realizados < intentos_maximos:
     intentos_realizados += 1
     
     if intento == numero_secreto:
+        puntuacion = (intentos_maximos - intentos_realizados + 1) * 100
         print(f"¡Felicidades! {intento} era el número secreto.")
+        print(f"Puntuacion: {puntuacion}")
         break 
     if intento < numero_secreto:
-        print(f"'{intento}' es muy bajo.")
+        print(f"'{intento}' es muy bajo. ↓")
     else:
-        print(f"'{intento}' es muy alto.")
+        print(f"'{intento}' es muy alto. ↑")
 
 if intento != numero_secreto:
     print(f"GAME OVER. El número era {numero_secreto}.")
+    print("Puntuacion: 0 ptos. Mejor suerte a la proxima.")
