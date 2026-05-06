88db7adec1a829717fde7e971e26a8b564a70d9b
Tomás Canales <tomas.canales.mella@gmail.com>
2026-05-06 12:58:59 -0400

    nuevo archivo

---
+++ PYcodes/Ejercicios-SimulacroPrueba/Ejercicio3-simulacro.py
@@ -0,0 +1,11 @@
+FLETE_BASE = 5000
+CARGO_MENOR_500 = 100
+CARGO_500_A_1000 = 300
+CARGO_MAYOR_1000 = 500
+recargo = int(input("Ingresa los kilos de carga: "))
+
+print("====== DESPACHO ======")
+print(f"""
+    Monto fijo flete: $ {FLETE_BASE}
+
+""")
