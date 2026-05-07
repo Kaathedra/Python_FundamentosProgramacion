81cf4f3f9615052be7f017e60982ec8b1bda6f3c
Tomás Canales <tomas.canales.mella@gmail.com>
2026-05-07 14:54:28 -0400

    Nuevo ejercicio en proceso

---
+++ Listado_Preparacion-ev2/Ex3_InstallInternet.py
@@ -0,0 +1,11 @@
+PLAN_DUO = 25000
+INSTALACION = 15000
+
+zona = str(input("Ingrese zona(Urbana/Rural): ".strip().lower()))
+
+es_cliente = str(input("Es cliente previo? (s/n): ".strip().lower()))
+
+estrato_valido = (1, 2, 3, 4, 5, 6)
+
+estrato_client = int(input("Ingrese su estrato (1-6): ".strip().lower()))
+
