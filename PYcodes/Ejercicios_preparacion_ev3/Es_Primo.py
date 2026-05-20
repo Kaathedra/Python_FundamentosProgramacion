6a9d0d7041fb85ab16040940e329ec39f4a5dd7b
Tomas Canales <tomas.canales.mella@gmail.com>
2026-05-20 15:43:29 -0400

    Nuevo Ejercicio "Es_Primo"

---
+++ PYcodes/Ejercicios_preparacion_ev3/Es_Primo.py
@@ -0,0 +1,38 @@
+import math
+
+def es_primo(numero):
+    if numero <= 1:
+        return False
+    
+    for i in range(2, int(math.isqrt(numero)) + 1):
+        if numero % i == 0:
+            return False
+    return True
+
+while True:
+    try:
+        cant_num = int(input("Ingresa la cantidad de numeros a verificar: "))
+        if type(cant_num) == int:
+            break
+    except ValueError:
+        print("Error: Ingresa solo numeros enteros.")
+
+primo = []
+
+no_primo = []
+
+for i in range(cant_num):
+    while True:
+        try:
+            numero = int(input("Ingrese el numero a verificar: "))
+            if es_primo(numero):
+                primo.append(numero)
+            else:
+                no_primo.append(numero)
+            break
+        except ValueError:
+            print("Error: Ingresa un numero entero valido.")
+
+print("\n--- Resultados ---")
+print(f"Se ingresaron {len(primo)} numeros primos. {primo}")
+print(f"Se ingresaron {len(no_primo)} numeros no primos. {no_primo}")
